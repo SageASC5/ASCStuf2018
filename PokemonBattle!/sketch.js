4e4db00be7878e48e9ab2e43b14811b4f6941eb0
SageASC5 <sagegreen15@gmail.com>
2018-07-24 11:45:37 -0400

    tomfoolery

--- PokemonBattle!/sketch.js
+++ PokemonBattle!/sketch.js
@@ -67,11 +67,23 @@ function draw(){
     triangle(365,125,505, 125, 428, 255)    
     fill(0)
     text("Charmander", 350, 90, 50, 30)
+    //HealthBar1
     fill(color)
-    rect(800-healing,445,health,33);
+    rect(800,445,health,33);
+    //HealthBar2
     fill(color2)
     rect(340,250,health2,33);
+    //Message Box
+    strokeWeight(3)
+    fill(255)
+    rect(350,355, 375, 110)
+    textSize(14)
+    fill(0)
+    text(squirtleText + " " + charmanderText,355,360, 370, 140)
 }
+
+let squirtleText ="An amazing fight has begun!"
+let charmanderText = "Who will win?!?"
 
 //https://wireframe.cc/5SqtRw
 function mouseClicked(){
@@ -88,29 +100,50 @@ function mouseClicked(){
     let damageFlamethrower = 60
     let damageEmber = 20
 
+    
     //Bubble
-    if(mouseX < 670 && 350< mouseX && mouseY < 600 && 500< zmouseY && bubblePP>0 && accuracyBubble<=7){
-        health2 = health2 - damageBubble
+    if(mouseX < 670 && 350< mouseX && mouseY < 600 && 500< mouseY && bubblePP>0){
         bubblePP--;
-       
-    }//Hydro Pump
-    else if(mouseX < 670 && 350< mouseX && mouseY < 715 && 615< mouseY && hydroPumpPP>0 && accuracyHydroPump<=5){
-        health2 = health2 - damageHydroPump
+        if(accuracyBubble<=7){
+            health2 = health2 - damageBubble
+            squirtleText ="Squirtle used Bubble! The physical attack of the bubbles hitting Charmander doesn't do much, but the popping startled him, dealing " + damageBubble + " to Charmander. "
+        }else {
+            squirtleText ="Squirtle used Bubble! But it missed and floated harmlessly towards the sky. "
+        }
+       }
+    
+    //Hydro Pump
+    else if(mouseX < 670 && 350< mouseX && mouseY < 715 && 615< mouseY && hydroPumpPP>0){
+        if(accuracyHydroPump<=5){
+            health2 = health2 - damageHydroPump
+            squirtleText = "Squirtle used Hydro Pump! A torrent of water shot from Squirtle's mouth, slamming into and dealing " + damageHydroPump + " to Charmander!"}
+        else {
+            squirtleText = "Squirtle used Hydro Pump! But her pipes were clogged and now she just has a steady drip of water leaking from her mouth."
+        }
         hydroPumpPP--;
-    }//Withdraw
+    }
+    
+    //Withdraw
     else if(mouseX < 998 && mouseX >678 && mouseY < 600 && mouseY > 500 && withdrawPP>0){
         if (health < 178){
-            healing = 30
+            healing = 60
             health = health + healing
             withdrawPP--
-        }else if(health<208 && health >=178){
+            squirtleText = "Squirtle uses Withdraw! She tucks her arms and head into her shell, healing for " + healing + " health."
+        }else if(health<208){
             health = 208
-        }else{
-
+            healing = 208 - health
+            withdrawPP--
+            squirtleText = "Squirtle uses Withdraw! She tucks her arms and head into her shell, healing for " + healing + " health."
+        }else {
+            squirtleText = "Squirtle uses Withdraw! She tucks her arms and head into her shell, but having full health she heals for 0 health."    
         }
-    }//Splash
+    }
+    
+    //Splash
     else if(mouseX < 998 && mouseX > 678 && mouseY < 715 && 615< mouseY && splashPP){
         splashPP--
+        squirtleText = "Squirtle used Splash! Nothing really happened..."
     }
     
     //healthbar1
@@ -136,31 +169,58 @@ function mouseClicked(){
     let chosenAttack = random(enemyAttacks)
     console.log(chosenAttack)
     //flamethrower
-    if (chosenAttack = enemyAttacks[0] && health2>0){
+    if (health2 <= 0){
+        charmanderText = "Charmander has been defeated! Squirtle is victorious!"
+    }else if (chosenAttack == enemyAttacks[0] && health2>0){
         if (accuracyFlamethrower<=4){
-            health = health - damageFlamethrower;}
+            health = health - damageFlamethrower;
+            charmanderText = "Charmander used Flamethrower, breathing a stream of fire from his mouth and dealing " + damageFlamethrower + " to Squirtle."
+        }else {
+            charmanderText = "Charmander used Flamethrower, but nothing but soot and smoke came out of his mouth."
+        }
         flamethrowerPP--;
     //ember
-    }else if (chosenAttack = enemyAttacks[2] && health2>0){
+    }else if (chosenAttack == enemyAttacks[2] && health2>0){
        if (accuracyEmber<=7){
-           health = health - damageEmber;}
+           health = health - damageEmber;
+           charmanderText = "Charmander used Ember, shooting a quick, small fireball towards his opponent, dealing " + damageEmber + " damage to Squirtle."
+       }else {
+           charmanderText = "Charmander used Ember, but the flame only traveled a couple inches before dissapating, leaving Charmander embarrassed and Squirtle amused."
+       }
        emberPP--; 
     //smokescreen
-    }else if (chosenAttack = enemyAttacks[1] && health2>0){
+    }else if (chosenAttack == enemyAttacks[1] && health2>0){
         if (accuracySmokescreen<=5){
-            if(Math.floor(random(1,2)) = 1){
+            if(Math.floor(random(1,2)) == 1){
                 accuracyBubble = accuracyBubble - 1
-            }else {accuracyHydroPump = accuracyHydroPump - 1
+                charmanderText = "Charizard used Smokescreen, surrounding Squirtle in a cloud of smoke and ash! Squirtle's Bubble's accuracy was decreased by 10%."
+            }else {
+                accuracyHydroPump = accuracyHydroPump - 1
+                charmanderText = "Charizard used Smokescreen, surrounding Squirtle in a cloud of smoke and ash! Squirtle's Hydro Pump's accuracy was decreased by 10%."
             }
         }smokescreenPP--;
     //growl
-    }else if (chosenAttack = enemyAttacks[1] && health2>0){
+    }else if (chosenAttack == enemyAttacks[3] && health2>0){
         if (accuracyGrowl<=6){
-            if (Math.floor(random(1,2)) = 1){
+            if (Math.floor(random(1,2)) == 1){
                 damageBubble = damageBubble*.8 
+                charmanderText = "Charizard used Growl, frightening Squirtle and decreasing the damage of her Bubble by 20%!"
             }else {
                 damageHydroPump = damageHydroPump*.8
+                charmanderText = "Charizard used Growl, frightening Squirtle and decreasing the damage of her Hydro Pump by 20%."
             }
-        }growlPP--;
+        }else {
+            charmanderText = "Charizard used Growl, but ended up with nothing but a voice crack!"
+        }
+        growlPP--;
     }
+
+    strokeWeight(3)
+    fill(255)
+    rect(350,395, 375, 70)
+    fill(0)
+    text(squirtleText + " " + charmanderText,355,400, 400, 100)
 }
+
+
+//add way to restart, like click to play again
